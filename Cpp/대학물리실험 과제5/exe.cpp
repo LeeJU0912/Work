61255e08bd4ec72581f0b221e1b2aad31772fdcf
LeeJU <101004585+LeeJU0912@users.noreply.github.com>
2022-09-07 02:40:54 +0900

    대학물리실험 소스코드

---
+++ Cpp/대학물리실험 과제5/exe.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+	int i;
+	double a[5];
+	double b[5];
+	double c[5];
+	double sum = 0;
+	double g;
+
+	cout << "-------------------------------------" << '\n';
+	cout << "대학물리실험 5. 중력가속도측정 보고서" << '\n';
+	cout << "-------------------------------------" << '\n';
+
+	for (i = 0; i < 5; i++) {
+		cout << i + 1 << "회 - v1을 입력하세요 : ";
+		cin >> a[i];
+		cout << i + 1 << "회 - v2를 입력하세요 : ";
+		cin >> b[i];
+		c[i] = ((b[i] * b[i]) - (a[i] * a[i])) / 1.6;
+		sum += c[i];
+	}
+	cout << "\n" << '\n';
+
+	for (i = 0; i < 5; i++) {
+		cout << i + 1 << "회 a값은 " << round(c[i] * 10000) / 10000 << " 입니다" << '\n';
+	}
+	sum = sum / 5;
+
+	g = sum / (0.0225 / 1);
+
+	cout << "\na의 평균은 " << round(sum * 10000) / 10000 << " 입니다" << '\n';
+
+	cout << "\ng의 실험값은 " << round(g * 1000) / 1000 << " 입니다" << '\n';
+
+	_getch();
+	return 0;
+}
---
+++ Cpp/대학물리실험 과제7/exe.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+	int i;
+	string f = "1";
+	double e;
+	double m1;
+	double m2;
+	double a[3];
+	double b[3];
+	double c[3];
+	double d[3];
+	double sum = 0;
+
+	while (f != "0") {
+		cout << "-------------------------------------" << '\n';
+		cout << "대학물리실험 7. 뉴턴 2법칙 보고서 V3" << '\n';
+		cout << "-------------------------------------" << '\n';
+
+		cout << "m1을 입력하세요 : ";
+		cin >> m1;
+		cout << "m2를 입력하세요 : ";
+		cin >> m2;
+
+		cout << '\n';
+
+		for (i = 0; i < 3; i++) {
+			cout << i + 1 << "회 - t3를 입력하세요 : ";
+			cin >> c[i];
+			cout << i + 1 << "회 - V1을 입력하세요 : ";
+			cin >> a[i];
+			cout << i + 1 << "회 - V2를 입력하세요 : ";
+			cin >> b[i];
+
+			cout << '\n';
+
+			d[i] = (b[i] - a[i]) / c[i];
+			sum += d[i];
+		}
+
+		cout << fixed << setprecision(3);
+
+		for (i = 0; i < 3; i++) {
+			if (d[i] < 1) {
+				cout << fixed << setprecision(4);
+				cout << i + 1 << "회 a값은 " << d[i] << " 입니다" << '\n';
+				cout << fixed << setprecision(3);
+			}
+			else {
+				cout << i + 1 << "회 a값은 " << d[i] << " 입니다" << '\n';
+			}
+
+		}
+		sum = sum / 3;
+
+		e = m2 / (m1 + m2) * 9.8;
+
+		if (sum < 1) {
+			cout << fixed << setprecision(4);
+			cout << "\n실험 a의 평균은 " << sum << " 입니다" << '\n';
+		}
+		else {
+			cout << "\n실험 a의 평균은 " << sum << " 입니다" << '\n';
+		}
+
+		cout << fixed << setprecision(1);
+		if (e < 1) {
+			cout << fixed << setprecision(2);
+			cout << "\n이론 a값은 " << e << " 입니다" << '\n';
+		}
+		else {
+			cout << "\n이론 a값은 " << e << " 입니다" << '\n';
+		}
+
+		cout << fixed << setprecision(0);
+
+		cout << "\na값 오차는 " << (e - sum) / e * 100 << "% 입니다" << '\n';
+
+		cout << "\n아무 키나 입력하면 다시 시작됩니다." << '\n';
+		cout << "0 을 입력하면 종료됩니다." << '\n';
+		cout << "입력 (0을 입력하면 종료) : ";
+		cin >> f;
+
+		cout << '\n';
+		cout << '\n';
+
+		
+	}
+
+	return 0;
+}
